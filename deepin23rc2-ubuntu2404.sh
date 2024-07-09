be3ac780e2c054e9bb7cba12ce8d05a6f08900fa
RicerVM <138690051+kota-rina3@users.noreply.github.com>
2024-07-09 09:14:47 +0800

    polished
    polished

--- deepin23rc2-ubuntu2404.sh
+++ deepin23rc2-ubuntu2404.sh
@@ -14,9 +14,9 @@ curl -o /home/${USER}/Desktop/libsdl2-image-2.0-0_2.8.2+dfsg-1+b1_amd64.deb -# h
 sudo chmod 777 *.deb
 echo -e "\033[36m开始安装相关软件包及其依赖......\033[0m"
 sleep 0.5
-sudo apt install -y './libjpeg62-turbo_2.1.5-3_amd64.deb' './libpng16-16t64_1.6.43-5_amd64.deb' './libsdl2-image-2.0-0_2.8.2+dfsg-1+b1_amd64.deb'
-sudo apt install -y './python3-renpy_8.2.2+dfsg-1_amd64.deb' './python3-pygame-sdl2_8.2.2-1_amd64.deb'
-echo -e "\033[36m送归缓存......\033[0m"
+sudo apt install -y /home/${USER}/Desktop/lib*.deb
+sudo apt install -y /home/${USER}/Desktop/python3-*.deb
+echo -e "\033[36m清理缓存......\033[0m"
 sleep 0.5
 sudo rm -rf /home/${USER}/Desktop/*.deb
 echo -e "\033[36m搞定！\033[0m"
